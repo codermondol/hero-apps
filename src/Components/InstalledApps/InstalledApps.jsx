9b088ebb48a6889e5022263bf630aa5ae36a0ab0
my first github repo <mrarifmondol98@gmail.com>
2026-01-14 06:39:11 -0800

    added localstorage function

--- src/Components/InstalledApps/InstalledApps.jsx
+++ src/Components/InstalledApps/InstalledApps.jsx
@@ -1,25 +1,46 @@
-import { Download } from 'lucide-react';
-import React from 'react';
+import { Download, Star } from 'lucide-react';
+import React, { useState } from 'react';
 import { formatDownloads } from '../../utility/FormatDownload';
+import { removeFromStoredLS } from '../../utility/addToDB';
 
-const InstalledApps = ({installedItem}) => {
-    
-    const {image, downloads} = installedItem;
+const InstalledApps = ({ installedItem }) => {
+    const [isInstalled, setIsInstalled] = useState(true);
 
+    const { image, downloads, ratingAvg, size, id } = installedItem;
+
+    const handleUninstall = (id) => {
+
+        removeFromStoredLS(id)
+        setIsInstalled(false);
+    }
+    if (!isInstalled) {
+        return null; 
+    }
 
     return (
-        <div>
-            <div>
-                <div>
-                    <img src={image} alt="" />
+        <div className='bg-white rounded mb-5 p-5'>
+            <div className='flex justify-between items-center'>
+                <div className='flex gap-7'>
+                    <div className='max-w-[100px] max-h-[100px]'>
+                        <img src={image} alt="" className='w-auto h-auto max-w-full max-h-full rounded object-contain' />
+                    </div>
+                    <div className='space-y-5'>
+                        <h4 className='text-xl font-bold'>Forest; focus for Productive</h4>
+                        <div className='flex items-center gap-5'>
+                            <div className='flex gap-2 font-bold text-[#00D390]'>
+                                <span><Download></Download></span>
+                                <span>{formatDownloads(downloads)}</span>
+                            </div>
+                            <div className='flex gap-2 font-bold text-[#FF8811]'>
+                                <span><Star></Star></span>
+                                <span>{ratingAvg}</span>
+                            </div>
+                            <div className='font-bold text-[#627382]'>{size} MB</div>
+                        </div>
+                    </div>
                 </div>
                 <div>
-                    <h4>Forest; focus for Productive</h4>
-                    <div>
-                        <div><span><Download></Download></span> {formatDownloads(downloads)}</div>
-                        <div></div>
-                        <div></div>
-                    </div>
+                    <button onClick={() => handleUninstall(id)} className='btn bg-[#00D390] hover:bg-gradient-to-br hover:from-[#632EE3] hover:to-[#9F62F2] text-white text-xl py-7 rounded transition-all duration-300'>Uninstall</button>
                 </div>
             </div>
         </div>
--- src/utility/addToDB.js
+++ src/utility/addToDB.js
@@ -1,22 +1,37 @@
 const getStoredApps = () => {
     const storedAppsSTR = localStorage.getItem('installed');
-    if(storedAppsSTR){
+    if (storedAppsSTR) {
         const storedAppData = JSON.parse(storedAppsSTR);
         return storedAppData
     }
     return []
 }
 
+const saveAppToLS = data => {
+    localStorage.setItem('installed', data)
+}
+
 const addToStoredLS = (id) => {
     const storedAppData = getStoredApps();
-    if(storedAppData.includes(id)){
+    if (storedAppData.includes(id)) {
         alert('This item already Exist')
     }
-    else{
+    else {
         storedAppData.push(id);
         const data = JSON.stringify(storedAppData)
-        localStorage.setItem('installed', data)
+        saveAppToLS(data)
     }
 }
 
-export {addToStoredLS, getStoredApps}
+const removeFromStoredLS = (id) => {
+  const storedAppData = getStoredApps();
+
+  const remainingData = storedAppData.filter(
+    appId => String(appId) !== String(id)
+  );
+
+  saveAppToLS(remainingData);
+};
+
+
+export { addToStoredLS, getStoredApps, removeFromStoredLS }
